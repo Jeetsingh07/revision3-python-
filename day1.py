2ed4e4d44883e46736cc228ce4f84ccda1011ef3
Jeetsingh07 <jeetkumarsingh07india@gmail.com>
2025-09-22 23:41:04 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,85 @@
+# a=int(input("enter first number ") )
+# b=int(input("enter second number ") )
+# c=int(input("enter third number ") )
+# print("sum is ",a+b+c)
+# print("subtraction is ",a-b-c)
+# print("multiplication is ",a*b*c)
+# print ("division is ",a/b/c)
+
+
+
+# a= int(input("enter first number ") )
+# # b= int(input("enter second number ") )
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+
+
+# a=int(input("enter radius "))
+# area=3.14*a*a
+# print("area is ",area)
+# circumferance=2*3.14*a
+# print("circumferance is ",circumferance)
+
+
+
+
+# a=int(input("first number "))
+# b=int(input("enter secind number "))
+# if a>b:
+#     print("a is greater than b")
+# else:
+#     print("b is geater than a")
+
+
+# p = int(input("enter principle amount "))
+# r=int (input("enter rate of interest "))
+# t=int (input("enter time in years "))
+# print("simple interest is ",(p*r*t)/100)
+
+
+
+
+# a=int(input("enter first number "))
+# b=int(input("enter second number "))
+# a=b
+# b=a
+# temp=a
+# print("after swapping first number is ",a)
+# print("after swapping second number is ",b)
+
+
+
+
+
+
+
+# a=int(input("enter first number ") )
+# b=int(input("enter second number ") )
+# c=int(input("enter third number ") )
+# print("sum is ",a+b+c)
+# print("subtraction is ",a-b-c)
+# print("multiplication is ",a*b*c)
+# print ("division is ",a/b/c)
+
+
+
+# print(3 * (1 + 2)**2 - (2**2) * 3)
+
+# password="jeet"
+# input_password=input("enter your password ")
+# while input_password!=password:
+#     print("wrong password")
+#     input_password=password=input("enter your password ")
+# print("correct password")
+
+password = "jeet"
+input_password = input("Enter your password: ")
+
+while input_password != password:
+    print("Wrong password")
+    input_password = password = input("enter your password ")
+print("Correct password")
